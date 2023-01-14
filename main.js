8166fa9a18fbbaf9e9a2efe348f4c56d0bdbf80c
Uraniumking007 <96092183+Uraniumking007@users.noreply.github.com>
2023-01-14 12:11:58 +0530

    adding files

---
+++ main.js
@@ -0,0 +1,19 @@
+const inputExpense = document.getElementById('expense-amount');
+const listContainer = document.getElementById('expense-list-group');
+const addBtn = document.getElementById('add');
+const typeExpense = document.getElementById('inputGroupSelect03');
+
+let spent = 0;
+
+addBtn.addEventListener('click', () => {
+  let expense = Number(inputExpense.value);
+  let li = document.createElement('li');
+  li.classList.add('list-group-item');
+  li.classList.add('rounded');
+  li.classList.add('m-1');
+  li.textContent = `Amount : ${expense} Type : ${typeExpense.value}`;
+  listContainer.appendChild(li);
+  //   console.log(expense);
+  spent += expense;
+  document.getElementById('total-expense').innerText = spent;
+});
