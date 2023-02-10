4de0b930dc796c5e49e055b30617cfe04997c8a2
Uraniumking007 <96092183+Uraniumking007@users.noreply.github.com>
2023-02-10 15:16:49 +0530

    started using tailwinds css

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        space: '#EEEEEE',
+        teal: '#00ADB5',
+        grey: '#393E46',
+        black: '#222831',
+      },
+    },
+  },
+  plugins: [],
+};
